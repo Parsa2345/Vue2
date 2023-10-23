e7f606a661ed4b04348452e85f8b3b85c0ce406c
Parsa2345 <jack.kamard@gmail.com>
2023-10-23 12:11:05 +0330

    Myfirst cli app

---
+++ my-first-cli/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <Hello :name="name" :family="family" :phone="33454" @nameClicked="test"  />
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import Hello from './components/Hello.vue'
+
+export default {
+  data() {
+    return {
+      name:"Mohsen",
+      family:"Parsa",
+    }
+  },
+  methods: {
+    test(text){
+      console.log("object   "+text );
+    }
+
+
+  },
+  name: 'App',
+  components: {
+    // HelloWorld,
+    Hello
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ my-first-cli/src/components/Hello.vue
@@ -0,0 +1,19 @@
+<template>
+    <h1 @click="myAlert">{{name}}</h1>
+    <h1>{{family}}</h1>
+    <h1>{{phone}}</h1>
+</template>
+
+<script>
+export default {
+    name: "Hello",
+    props: { name: String, family: String, phone: Number },
+    methods: {
+        myAlert()
+        {
+            alert("Click Happened");
+            this.$emit("nameClicked",this.name)
+        }
+    },
+};
+</script>
